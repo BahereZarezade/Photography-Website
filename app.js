cc8ec443c50bcbdf13f304db95c38a71be6f4717
Bahere Zarezade <87176183+BahereZarezade@users.noreply.github.com>
2021-09-11 22:00:57 +0430

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const nav = document.querySelector(".nav-link");
 const burger = document.querySelector(".burger");
 const links = nav.querySelectorAll("a");
 
+//for  menu burger
 burger.addEventListener("click", () => {
   nav.classList.toggle("nav-open");
   burger.classList.toggle("toggle");
 });
 
+
+//for link:Home; About ,...
 links.forEach(link => {
   link.addEventListener("click", () => {
     nav.classList.toggle("nav-open");
